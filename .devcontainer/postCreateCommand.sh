b5f5c225a1e12cf742fd38d0293cbe3e6e611b45
inovue <inouve3@gmail.com>
2023-07-28 16:53:48 +0900

    add devcontaine & start badge

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# postCreateCommand.sh
+
+echo "START Install"
+
+sudo chown -R vscode:vscode .
+
+
+echo "First of all, please execute the following command"
+echo "-----------------------------------------------"
+echo "git config --global user.name \"Your Name\""
+echo "git config --global user.email \"Your Email\""
+echo "-----------------------------------------------"
+
+cp .env.local.example .env.local
+npm i
+
+echo "FINISH Install"
